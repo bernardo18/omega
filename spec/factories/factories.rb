3a426c2829e4d5d64e7a56323acc7f6252bfae60
Thunder <rcastro@yellowspot.com.ar>
2014-02-24 13:08:03 -0300

    factory para users

---
+++ spec/factories/factories.rb
@@ -0,0 +1,10 @@
+#encoding: utf-8
+FactoryGirl.define do
+  factory :user do |f|
+    #f.password "pass"
+    #f.password_confirmation "pass"
+    #sequence(:email) { |n| "email#{n}@example.com" }
+    f.sequence(:name) {|n| "Usuario #{n}"}
+    #f.association :aliance
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,15 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe "user_profile" do
+  it "factory user valid" do
+    user = FactoryGirl.build(:user)
+    user.should be_valid
+    user.save!
+  end
+
   it "validate presence name" do
-    user = User.new
+    user = FactoryGirl.build(:user)
+    user.name = nil
     user.should_not be_valid    
   end
 end
