033527a6e988974a28332c808a340c9fe991074f
bernardo <berny18@gmail.com>
2014-03-31 13:51:35 +0200

    corregi el test de static page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,33 +2,33 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  subject {pages}
+  subject { page }
 
   describe "Home page" do
     before { visit root_path }
 
     it { should have_content('Omega') }
-    it { should have_content(full_title'') }
+    it { should have_title(full_title'') }
   end
 
   describe "Help page" do
     before { visit help_path }
 
     it { should have_content('Ayuda') }
-    it { should have_content(full_title'Ayuda') }
+    it { should have_title(full_title'Ayuda') }
   end
 
   describe "About page" do
     before { visit about_path }
 
-    it { should have_content('Acerca de') }
-    it { should have_content(full_title'Acerca de nosotros') }
+    it { should have_content('Sobre nosotros') }
+    it { should have_title(full_title'Sobre nosotros') }
   end
 
   describe "Contact page" do
     before { visit contact_path }
 
     it { should have_content('Contacto') }
-    it { should have_content(full_title'Contacto') }
+    it { should have_title(full_title'Contacto') }
   end
 end
