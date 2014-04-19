8d379f3f8ed4fecdb954e2c95b1803fd4f15bc18
bernardo <berny18@gmail.com>
2014-04-19 16:05:23 +0200

    refactorice helper y header ignorar archivos .swo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,6 @@
 module ApplicationHelper
   def full_title(page_title)
     base_title = "Omega la travecia apenas empiesa"
-    if page_title.empty?
-      base_title
-    else
-      "#{base_title} | #{page_title}"
-    end
+    page_title.empty? ? base_title : "#{base_title} | #{page_title}"
   end
 end
